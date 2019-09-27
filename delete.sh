76e007c72c2cfdd030bba8b28743a2d1f19170aa
Vidyasagar Machupalli <vidyasagarmsc@gmail.com>
2019-09-27 13:51:31 +0530

    Updated the permissions

--- delete.sh
+++ delete.sh
@@ -3,11 +3,11 @@ if [[ -z "$1" ]]; then
     environments=('development' 'testing' 'production' 'cicd')
     for env in "${environments[@]}"; do
         echo "DELETING RESOURCES in ${env}..."
-        oc delete all -l app=hellonodeapp -n $env
+        oc delete all -l app=hello-node-app -n $env
         oc delete project $env
     done
     elif [[ -n "$1" ]]; then
     echo "DELETING RESOURCES in $1..."
-    oc delete all -l app=hellonodeapp -n $1
+    oc delete all -l app=hello-node-app -n $1
     oc delete project $1
 fi
