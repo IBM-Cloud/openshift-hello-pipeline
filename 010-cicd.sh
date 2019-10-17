1e6b140f88e078acf4494befad84aac54d0797be
Vidyasagar Machupalli <vidyasagarmsc@gmail.com>
2019-10-17 15:30:54 +0530

    Added project git

--- 010-cicd.sh
+++ 010-cicd.sh
@@ -6,7 +6,7 @@ oc new-project cicd
 oc adm policy add-cluster-role-to-user self-provisioner -z jenkins
 
 # create a new app pointing to the repo with Jenkinsfile
-oc new-app https://github.com/IBM-Cloud/hello-node-app-pipeline.git
+oc new-app https://github.com/IBM-Cloud/hello-node-app-pipeline.git#project
 
 # add policies allowing access to other projects
 oc policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n development
