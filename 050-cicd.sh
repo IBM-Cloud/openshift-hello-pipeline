6fcc7fd4f1f61b98f1a938e5d6bcbf07c574f57c
Vidyasagar Machupalli <vidyasagarmsc@gmail.com>
2019-10-18 12:06:48 +0530

    Updated pipeline script

--- 050-cicd.sh
+++ 050-cicd.sh
@@ -2,10 +2,10 @@
 # create a new project
 oc new-project cicd
 
+# create a new app pointing to the repo with Jenkinsfile
+oc new-app https://github.com/IBM-Cloud/hello-node-app-pipeline.git
+
 # add policies allowing access to other projects
 oc policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n development
 oc policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n testing
 oc policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n production
-
-# create a new app pointing to the repo with Jenkinsfile
-oc new-app https://github.com/IBM-Cloud/hello-node-app-pipeline.git
